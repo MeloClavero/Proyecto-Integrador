c0a214f2a66a677624e5e374d32f966603e98f55
Melina Clavero <meme.clavero@gmail.com>
2023-08-21 18:54:07 -0300

    Agrego Pagina Smartphones

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
-export default {
-    server: {
-        port: "2222",
+import{resolve} from 'node:path'
+//colsole.log(resolve('pages'))
+
+export default{
+    server:{
+        port:"2222",
     },
-    css: {
-        devSourcemap: true,
+    css:{
+        devSourcemap:true,
+        rollupOptions:{
+            input:{
+                smartphones:resolve('pages/smartphones.html'),
+                index:resolve('index.html')
+            }
+        }
     }
 }
