831fc264c401269d81257143cd69c9d4b6e24f40
Melina Clavero <meme.clavero@gmail.com>
2023-08-22 15:24:34 -0300

    Agrego paginas de descripcion de productos

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,23 @@ export default{
                 index:resolve('index.html'),
                 accesorios:resolve('pages/accesorios.html'),
                 computacion:resolve('pages/computacion.html'),
-                carrito:resolve('pages/carrito.html')
+                carrito:resolve('pages/carrito.html'),
+                productos1:resolve('pages/productos/accesorios-1.html'),
+                productos2:resolve('pages/productos/accesorios-2.html'),
+                productos3:resolve('pages/productos/accesorios-3.html'),
+                productos4:resolve('pages/productos/accesorios-4.html'),
+                productos5:resolve('pages/productos/accesorios-5.html'),
+                productos6:resolve('pages/productos/accesorios-6.html'),
+                productos7:resolve('pages/productos/smartphone-1.html'),
+                productos8:resolve('pages/productos/smartphone-2.html'),
+                productos9:resolve('pages/productos/smartphone-3.html'),
+                productos10:resolve('pages/productos/smartphone-4.html'),
+                productos11:resolve('pages/productos/smartphone-5.html'),
+                productos12:resolve('pages/productos/smartphone-6.html'),
+                productos13:resolve('pages/productos/computacion-1.html'),
+                productos14:resolve('pages/productos/computacion-2.html'),
+                productos15:resolve('pages/productos/computacion-3.html'),
+                productos16:resolve('pages/productos/computacion-4.html')
             }
         }
     }
